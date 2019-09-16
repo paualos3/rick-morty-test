159e09aee8b6b92c4ed02f6e09261656e3a0ce30
Pau Alos Mairal <paualos3@gmail.com>
2019-09-16 23:57:41 +0200

    Data and response models

--- app/src/main/java/com/example/paualos3/rick_morty_test/Models/InfoModel.java
+++ app/src/main/java/com/example/paualos3/rick_morty_test/Models/InfoModel.java
@@ -1,4 +1,44 @@
 package com.example.paualos3.rick_morty_test.Models;
 
 class InfoModel {
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    public String getPrev() {
+        return prev;
+    }
+
+    public void setPrev(String prev) {
+        this.prev = prev;
+    }
+
+    private int count;
+
+    private int pages;
+
+    private String next;
+    
+    private String prev ;
 }
