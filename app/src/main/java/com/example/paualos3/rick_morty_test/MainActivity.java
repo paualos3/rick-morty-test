27cd0c822d8ff7f913ed082e5c2d4fec67554bf6
Pau Alos Mairal <paualos3@gmail.com>
2019-09-16 22:21:18 +0200

    Background image + startButton

--- app/src/main/java/com/example/paualos3/rick_morty_test/MainActivity.java
+++ app/src/main/java/com/example/paualos3/rick_morty_test/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.paualos3.rick_morty_test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startButton = (Button) findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(view.getContext(), R.string.app_name, Toast.LENGTH_LONG);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
     }
 }
